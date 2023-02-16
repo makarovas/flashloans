9a77a590894869abc12a98e8bb96119909851a24
makarovas <makarofalex@gmail.com>
2023-02-16 16:54:31 +0200

    add v2 bot

--- run-arb.js
+++ run-arb.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+import { ChainId, Token, TokenAmount, Pair, Fetcher } from "@uniswap/sdk";
 
 const abis = require("./abis");
 const Web3 = require("web3");
@@ -19,41 +20,48 @@ const RECENT_PRICE = 230;
 const AMOUNT_ETH_WEI = web3.utils.toWei(AMOUNT_ETH.toString());
 const AMOUNT_DAI_WEI = web3.utils.toWei((AMOUNT_ETH * RECENT_PRICE).toString());
 
-web3.eth
-  .subscribe("newBlockHeaders")
-  .on("data", async (block) => {
-    if (block.timestamp) {
-      const curr = Date.now();
-      const point = new Date(
-        new Date(block.number).toLocaleString()
-      ).getMilliseconds();
+const init = async () => {
+  const [dai, weth] = await Promise.all(
+    [address.tokens.dai, address.tokens.weth].map((tokenAddr) =>
+      Fetcher.fetchTokenData(ChainId.MAINNET, tokenAddr)
+    )
+  );
+  web3.eth
+    .subscribe("newBlockHeaders")
+    .on("data", async (block) => {
+      if (block.timestamp) {
+        const curr = Date.now();
+        const point = new Date(
+          new Date(block.number).toLocaleString()
+        ).getMilliseconds();
 
-      const kyberResults = await Promise.all([
-        kyber.methods
-          .getExpectedRate(
-            address.tokens.dai,
-            "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
-            AMOUNT_DAI_WEI
-          )
-          .call(),
-        kyber.methods
-          .getExpectedRate(
-            "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
-            address.tokens.dai,
-            AMOUNT_ETH_WEI
-          )
-          .call(),
-      ]);
+        const kyberResults = await Promise.all([
+          kyber.methods
+            .getExpectedRate(
+              address.tokens.dai,
+              "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+              AMOUNT_DAI_WEI
+            )
+            .call(),
+          kyber.methods
+            .getExpectedRate(
+              "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
+              address.tokens.dai,
+              AMOUNT_ETH_WEI
+            )
+            .call(),
+        ]);
 
-      const kyberRates = {
-        buy: parseFloat(1 / (kyberResults[0].expectedRate / 10 ** 18)),
-        sel: parseFloat(kyberResults[1].expectedRate / 10 ** 18),
-        diff:
-          parseFloat(1 / (kyberResults[0].expectedRate / 10 ** 18)) -
-          parseFloat(kyberResults[1].expectedRate / 10 ** 18), 
-      };
+        const kyberRates = {
+          buy: parseFloat(1 / (kyberResults[0].expectedRate / 10 ** 18)),
+          sel: parseFloat(kyberResults[1].expectedRate / 10 ** 18),
+          diff:
+            parseFloat(1 / (kyberResults[0].expectedRate / 10 ** 18)) -
+            parseFloat(kyberResults[1].expectedRate / 10 ** 18),
+        };
 
-      console.log(kyberRates);
-    }
-  })
-  .on("error", (error) => console.log(error));
+        console.log(kyberRates);
+      }
+    })
+    .on("error", (error) => console.log(error));
+};
