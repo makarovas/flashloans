90a97262ddeb5707c0b6e2d41c0f3a314c680c59
makarovas <makarofalex@gmail.com>
2023-02-16 15:33:36 +0200

    add amounts of expected rates

--- run-arb.js
+++ run-arb.js
@@ -8,12 +8,16 @@ const web3 = new Web3(
   new Web3.providers.WebsocketProvider(process.env.MAINNET_INFURA)
 );
 
-const kyper = new web3.eth.Contract(
+const kyber = new web3.eth.Contract(
   abis.kyber.kyberNetworkProxy,
   address.kyber.kyberNetworkProxy
 );
 
-// console.log(kyper[1]);  
+// console.log(kyper[1]);
+const AMOUNT_ETH = 100;
+const RECENT_PRICE = 230;
+const AMOUNT_ETH_WEI = web3.utils.toWei(AMOUNT_ETH.toString());
+const AMOUNT_DAI_WEI = web3.utils.toWei((AMOUNT_ETH * RECENT_PRICE).toString());
 
 web3.eth
   .subscribe("newBlockHeaders")
@@ -24,7 +28,24 @@ web3.eth
         new Date(block.number).toLocaleString()
       ).getMilliseconds();
 
-      console.log(curr, new Date(block.number).toLocaleString(), point);
+      const kyberResults = await Promise.all([
+        kyber.methods
+          .getExpectedRate(
+            address.tokens.dai,
+            "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+            AMOUNT_DAI_WEI
+          )
+          .call(),
+        kyber.methods
+          .getExpectedRate(
+            "0xeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee",
+            address.tokens.dai,
+            AMOUNT_ETH_WEI
+          )
+          .call(),
+      ]);
+
+      console.log(kyberResults);
     }
   })
   .on("error", (error) => console.log(error));
